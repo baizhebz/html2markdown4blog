9ab49eb3c854e17e32405eb0fa7b0bdb87cb1f24
bai zhe <baizhebz@gmail.com>
2014-09-14 22:03:22 +0800

    add the comment for setting.php file

--- setting.php
+++ setting.php
@@ -6,13 +6,14 @@ return array(
     'blog_base_url' => 'http://blog.csdn.net',
 
     //需要转换的文章的url，下面的archive_url未设置时，该设置有效
-    'post_url' => 'http://blog.csdn.net/sky_zhe/article/details/9702489',
+    'post_url' => '',
 
-    //文章列表，当设置了这个，上面的post_url设置无效
-    //'archive_url' => 'http://blog.csdn.net/sky_zhe',
+    //目录列表的url，如果设置了这个，则上面的post_url设置无效
     'archive_url' => '',
+
     //是否在文章头部显示日志属性，包括title,date,tag等
     'inject_md_header' => true,
 
+    //是否使用github风格的code blocks的语法
     'github_code_block_style' => true
 );
